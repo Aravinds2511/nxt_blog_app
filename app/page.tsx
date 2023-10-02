0f7cefde982c0c024db5918c65779f7abb2945f6
arvndblocker"nn <aravindvasang@gmail.com>
2023-10-02 13:08:53 +0530

    blog_updates

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,22 @@
+import getCurrentUser from "./actions/getCurrentUser"
+import getBlogs from "./actions/getBlogs"
+import SingleBlog from "@/components/blog/SingleBlog";
+
+export default async function Home() {
 
 
-export default function Home() {
+  const currentUser = await getCurrentUser();
+  const blogs = await getBlogs()
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     
+       {blogs.map((item) => (
+        <SingleBlog
+          data={item}
+          key={item.id}
+          currentUser={currentUser}
+        />
+       ))}
     </main>
   )
 }
